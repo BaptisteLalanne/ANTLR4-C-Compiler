f2eab4b32705e0fc46f610c7e860b40b333ec70f
Baptiste Lalanne <baptiste.lalanne@outlook.fr>
2022-02-28 08:10:55 +0100

    :tada: Add template

---
+++ install-antlr.sh
@@ -0,0 +1,53 @@
+
+set -e # Abort the script at the first error
+
+######################################################################
+# Preparation step
+
+# create the target directory for installation
+mkdir antlr 
+
+# create a fresh temp directory
+mkdir antlr.tmp
+cd antlr.tmp
+
+#download both the tool jar and the cpp runtime
+curl -O https://www.antlr.org/download/antlr-4.9.2-complete.jar
+curl -O https://www.antlr.org/download/antlr4-cpp-runtime-4.9.2-source.zip
+
+######################################################################
+# Build step
+
+unzip *.zip
+mkdir build
+cd build
+cmake ..
+make
+
+cd ../..
+
+######################################################################
+# install step
+
+# tool jar 
+mkdir antlr/jar
+mv antlr.tmp/*.jar antlr/jar
+
+# runtime header files
+mkdir antlr/include
+
+cd antlr.tmp/runtime/src
+find * -type d -print -exec mkdir ../../../antlr/include/'{}' ';'
+find . -name '*.h' -print -exec cp '{}' ../../../antlr/include/'{}' ';'
+cd ../../..
+
+# runtime library files
+mkdir antlr/lib
+mv antlr.tmp/dist/libantlr4-runtime.a antlr/lib
+
+######################################################################
+# cleanup step
+
+rm -rf antlr.tmp
+
+echo PLD COMP: ANTLR OK
