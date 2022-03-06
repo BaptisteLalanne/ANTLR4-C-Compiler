a64aba726973192f5ede67359a9cca2502a2c876
AmL-Dev <aml1234@laposte.net>
2022-03-06 16:38:52 +0100

    :art: Commented the code

--- compiler/ErrorHandler.h
+++ compiler/ErrorHandler.h
@@ -1,10 +1,25 @@
+/*************************************************************************
+                          PLD Compiler : ErrorHandler
+                          ---------------------------
+    start   : 02/28/2022
+    authors : Bastien B. Laetitia D. Arthur D. Loann L.
+			  Baptiste L. Amine L. Tom P. David T.
+*************************************************************************/
+
+//------ Interface of class <ErrorHandler> (file ErrorHandler.h) -------/
 #pragma once
 
+//--------------------------------------------------- Called interfaces
 #include <string>
 using namespace std;
 
+//------------------------------------------------------------------ Types
 enum errorType {ERROR, WARNING};
 
+//------------------------------------------------------------------------
+// Goal of class <ErrorHandler>
+// The goal of this class is to handle errors and warnings
+//------------------------------------------------------------------------
 class ErrorHandler {
     
     public:
