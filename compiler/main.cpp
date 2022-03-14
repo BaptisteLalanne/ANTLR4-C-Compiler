7bd945862905f7f93a934196bb4f97c63bc93066
Marcus Toma <best4tdm@gmail.com>
2022-03-14 10:47:05 +0100

    :bug: Fix lexer errors

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -39,6 +39,11 @@ int main(int argn, const char **argv) {
 
     if(parser.getNumberOfSyntaxErrors() != 0) {
         cerr << "ERROR : syntax error during parsing" << endl;
+        cout.flush();
+        exit(1);
+    }
+    if(lexer.getNumberOfSyntaxErrors() != 0) {
+        cout.flush();
         exit(1);
     }
     
--- tests/testfiles/2_8_const_valid.c
+++ tests/testfiles/2_8_const_valid.c
@@ -1,4 +1,5 @@
 int main() {
     int x;
     x = 2147483648;
+    return x;
 }
