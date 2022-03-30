8c603829eef56ca522f732152c6abb2a1f731f55
AmL-Dev <aml1234@laposte.net>
2022-03-30 17:16:59 +0200

    ✅ New test for if statement

--- tests/testfiles/11_01_19_if_else_affect.c
+++
@@ -1,9 +0,0 @@
-int main() {
-    int a;
-    if (1 < a=4) {
-        a=2;
-    } else {
-        a=3;
-    }
-    return a;
-}
---
+++ tests/testfiles/11_01_19_if_if_else.c
@@ -0,0 +1,13 @@
+int main() {
+    int a = 1;
+    if (a==1) {
+        a=2;
+    } 
+    if (a==2) {
+        a=3;
+    }
+    else {
+        a=4;
+    }
+    return a;
+}
