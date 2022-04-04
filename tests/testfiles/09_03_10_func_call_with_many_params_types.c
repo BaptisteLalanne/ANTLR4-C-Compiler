46ede0d8c5b9f6315d2e1e29b9859678edf77b55
Marcus Toma <best4tdm@gmail.com>
2022-04-04 10:02:33 +0200

    :construction: fruitless attempts...

--- tests/testfiles/09_03_10_func_call_with_many_params_types.c
+++ tests/testfiles/09_03_10_func_call_with_many_params_types.c
@@ -5,8 +5,5 @@ int toto(char a, int b, int c, char d, int e, int f, int g, int h, char i) {
 int main() {
     char a = 'P';
     int c = 0;
-    while (c < 5) {
-        c = c+1;
-    }
-    return toto('a', 1, 5, 't', 12, c, 5, 79, a);
+    return toto('a', 1, 5, 't', 12, c, 5, 50, a);
 }
